a02f7b4bba25a414e50689091fd9f8ea40b99a06
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2021-09-28 16:03:27 -0400

    guiv1.0

--- gui.py
+++ gui.py
@@ -1,2 +1,9 @@
 from apis.control import Control
-from apis.control import control
+from apis.data import Data
+
+class GUI:
+    def __init__(self):
+        dat = Data()
+        
+gui = GUI()
+print(gui.dat)
