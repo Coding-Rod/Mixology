21d5651dfa414ab1fe8e318a64855e5a9ad6a87f
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2021-09-28 18:14:52 -0400

    Verification

--- gui.py
+++ gui.py
@@ -1,2 +1,7 @@
+from apis.data import Data
 from apis.control import Control
-from apis.control import control
+
+dat = Data()
+# dat.add_recipe("Corange","Coconut,Orange,Milk","100,100,300","Lemon,Orange",False)
+dat.change_box("5","Ice")
+print(dat.verify(1))
