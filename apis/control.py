91f9c522c0a781dd2f573895a0dc599a0cc4fbfc
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2021-10-01 22:50:33 -0400

    osva

--- apis/control.py
+++ apis/control.py
@@ -8,19 +8,20 @@ class Control:
         self.leds = [14,15,18,23,24,25,11] # Boxes and mixer
         self.pumps = [2,3,4,17,27,22,10,9] # Bottles
         self.vase = 26
+
         GPIO.setmode(GPIO.BCM)
         for i in (self.leds+self.pumps+[self.vase]): 
             GPIO.setup(i,GPIO.OUT)
             
-    def pump_control(self, selected, seconds):
+    def pump_control(self, selected, seconds): #bottles
         for i,j in selected,seconds:
-            GPIO.output(self.pumps.index(i), GPIO.HIGH)
+            GPIO.output(self.pumps.index(i-1), GPIO.HIGH)
             time.sleep(seconds)
-            GPIO.output(self.pumps.index(i), GPIO.LOW)
+            GPIO.output(self.pumps.index(i-1), GPIO.LOW)
 
-    def led_control(self, selected):
+    def led_control(self, selected): #boxes
         for i in selected:
-            GPIO.output(self.leds.index(i), GPIO.HIGH)
+            GPIO.output(self.leds.index(i-1), GPIO.HIGH)
         time.sleep(len(selected)*2)
         for i in selected:
             GPIO.output(self.leds.index(i), GPIO.HIGH)
--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@ from apis.data import Data
 from apis.control import Control
 
 dat = Data()
+ctr = Control()
+
 # dat.add_recipe("Corange","Coconut,Orange,Milk","100,100,300","Lemon,Orange",False)
 # dat.change_box("5","Ice")
 # print(dat.verify(1))
