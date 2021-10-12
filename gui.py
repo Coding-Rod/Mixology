abcc9aed0e3c3eeeb1bac698bbf4e7dd95bfe4fb
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2021-10-12 18:23:02 -0400

    not working gui

--- gui.py
+++ gui.py
@@ -47,18 +47,32 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.show_frame("PageTwo"))
-        button3 = tk.Button(self, text="Go to Page Three",
-                            command=lambda: controller.show_frame("PageThree"))
-        button1.pack()
-        button2.pack()
-        button3.pack()
+        canvas = tk.Canvas(bg = "#eeeeee",height = 380,width = 800,bd = 0,highlightthickness = 0,relief = "ridge")
+        canvas.place(x = 0, y = 0)
+        
+        img0 = tk.PhotoImage(file = f"Proxlight_Designer_Export/img0.png")
+        b0 = tk.Button(image = img0, borderwidth = 0, highlightthickness = 0, command = lambda: print("Pattosva"), relief = "flat")
+
+        b0.place(x = 535, y = 171, width = 180, height = 80)
+
+        img1 = tk.PhotoImage(file = f"Proxlight_Designer_Export/img1.png")
+        b1 = tk.Button(image = img1, borderwidth = 0, highlightthickness = 0, command = lambda: print("Pattosva"), relief = "flat")
+
+        b1.place(x = 310, y = 171, width = 180, height = 80)
+
+        img2 = tk.PhotoImage(file = f"Proxlight_Designer_Export/img2.png")
+        b2 = tk.Button(image = img2,borderwidth = 0,highlightthickness = 0,command = lambda: print("Pattosva"),relief = "flat")
+
+        b2.place(x = 85, y = 171,width = 179,height = 80)
+
+        background_img = tk.PhotoImage(file = f"Proxlight_Designer_Export/background.png")
+        background = canvas.create_image(400.0, 37.0,image=background_img)
+
+        img3 = tk.PhotoImage(file = f"Proxlight_Designer_Export/img3.png")
+        b3 = tk.Button(image = img3, borderwidth = 0, highlightthickness = 0, command = lambda: print("Pattosva"), relief = "flat")
+
+        b3.place(x = 725, y = 7,width = 60,height = 60)
 
 
 class PageOne(tk.Frame):
@@ -134,4 +148,6 @@ class PageThree(tk.Frame):
 
 if __name__ == "__main__":
     app = SampleApp()
+    app.geometry("800x380")
+    app.configure(bg = "#eeeeee")
     app.mainloop()
